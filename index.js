d18cb4a23ff088e73e999f30d4d5e8820af5d232
lucianoire <carolborromeo9@gmail.com>
2026-04-30 17:14:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,55 @@ const commands = [
   new SlashCommandBuilder()
     .setName('listing')
     .setDescription('Create listing')
-    .addUserOption(o => o.setName('buyer').setRequired(true))
-    .addChannelOption(o => o.setName('channel').setRequired(true))
-    .addIntegerOption(o => o.setName('quantity').setRequired(true))
-    .addStringOption(o => o.setName('item').setRequired(true))
-    .addStringOption(o => o.setName('mop').setRequired(true))
-    .addNumberOption(o => o.setName('price').setRequired(true))
-    .addStringOption(o => o.setName('status').setRequired(true))
-    .addUserOption(o => o.setName('seller').setRequired(true))
+
+    .addUserOption(o =>
+      o.setName('buyer')
+        .setDescription('Buyer')
+        .setRequired(true)
+    )
+
+    .addChannelOption(o =>
+      o.setName('channel')
+        .setDescription('Channel')
+        .setRequired(true)
+    )
+
+    .addIntegerOption(o =>
+      o.setName('quantity')
+        .setDescription('Quantity')
+        .setRequired(true)
+    )
+
+    .addStringOption(o =>
+      o.setName('item')
+        .setDescription('Item')
+        .setRequired(true)
+    )
+
+    .addStringOption(o =>
+      o.setName('mop')
+        .setDescription('Mode of payment')
+        .setRequired(true)
+    )
+
+    .addNumberOption(o =>
+      o.setName('price')
+        .setDescription('Price')
+        .setRequired(true)
+    )
+
+    .addStringOption(o =>
+      o.setName('status')
+        .setDescription('Order status')
+        .setRequired(true)
+    )
+
+    .addUserOption(o =>
+      o.setName('seller')
+        .setDescription('Seller')
+        .setRequired(true)
+    )
+
 ].map(c => c.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);

c78ff0d01cb52a305568663b889d59241c8ba254
lucianoire <carolborromeo9@gmail.com>
2026-04-30 22:25:02 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,61 @@
 require('dotenv').config();
+
 const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 
 const commands = [
   new SlashCommandBuilder()
     .setName('listing')
-    .setDescription('Create listing')
-    .addUserOption(o => o.setName('buyer').setRequired(true))
-    .addChannelOption(o => o.setName('channel').setRequired(true))
-    .addIntegerOption(o => o.setName('quantity').setRequired(true))
-    .addStringOption(o => o.setName('item').setRequired(true))
-    .addStringOption(o => o.setName('mop').setRequired(true))
-    .addNumberOption(o => o.setName('price').setRequired(true))
-    .addStringOption(o => o.setName('status').setRequired(true))
-    .addUserOption(o => o.setName('seller').setRequired(true))
-].map(c => c.toJSON());
+    .setDescription('Create purchase lineup listing')
+    .addUserOption(option =>
+      option.setName('buyer')
+        .setDescription('Buyer')
+        .setRequired(true)
+    )
+    .addChannelOption(option =>
+      option.setName('channel')
+        .setDescription('Channel')
+        .setRequired(true)
+    )
+    .addIntegerOption(option =>
+      option.setName('quantity')
+        .setDescription('Quantity')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('item')
+        .setDescription('Item name')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('mop')
+        .setDescription('Mode of payment')
+        .setRequired(true)
+    )
+    .addNumberOption(option =>
+      option.setName('price')
+        .setDescription('Price')
+        .setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('status')
+        .setDescription('Order status')
+        .setRequired(true)
+        .addChoices(
+          { name: 'noted', value: 'noted' },
+          { name: 'processing', value: 'processing' },
+          { name: 'completed', value: 'completed' }
+        )
+    )
+    .addUserOption(option =>
+      option.setName('seller')
+        .setDescription('Seller')
+        .setRequired(true)
+    ),
+
+  new SlashCommandBuilder()
+    .setName('queued')
+    .setDescription('Send queued order notice')
+].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
@@ -22,5 +64,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
     { body: commands }
   );
+
   console.log('Commands deployed');
 })();
